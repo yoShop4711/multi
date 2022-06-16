35d577cab2f703eec96cdb69b7eb05a930587964
can7wysiwyg <paulkssa@gmail.com>
2022-06-16 06:28:05 +0200

    first commit of the day

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -6,6 +6,52 @@ const authSeller = require("../middleware/authSeller");
 const authAdmin = require("../middleware/authAdmin");
 const asyncHandler = require("express-async-handler");
 const multer = require("multer");
+const { query } = require("express");
+
+
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filtering() {
+    const queryObj = { ...this.queryString }; //queryString = req.query
+    const excludedFields = ["page", "sort", "limit"];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
+
+    this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+
+    return this;
+  }
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 9;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -129,5 +175,22 @@ ProductRoute.delete('/api/delete_product/:id', verify, asyncHandler(async(req, r
 
 }))
 
+
+ProductRoute.get('/api/show_products', asyncHandler(async(req, res) => {
+
+  const features = new APIfeatures(Product.find(), req.query)
+        .filtering()
+        .sorting()
+        .paginating();
+
+      const products = await features.query;
+
+      res.json({
+        status: "success",
+        result: products.length,
+        products: products,
+      });
+
+}))
 
 module.exports = ProductRoute;
