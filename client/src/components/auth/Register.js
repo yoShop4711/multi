f754f638f6fb7845e8ac64da9483c11c13599bf6
can7wysiwyg <paulkssa@gmail.com>
2022-06-26 08:26:46 +0200

    registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,48 +1,99 @@
-import './auth.css'
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import "./auth.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
-    
-    return (
-        <div className="login_page">
-            <h2>Register</h2>
-            
-            <form >
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" placeholder="Enter your name" id="name" />
-                </div>
-                <div>
-                    <label htmlFor="name">username</label>
-                    <input type="text" placeholder="Enter your username" id="name" />
-                </div>
+  const [values, setValues] = useState({
+    fullname: '',
+    username: "",
+    email: "",
+    password: "",
+    location: "",
+  });
 
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setValues({...values, [name]:value})
 
-                <div>
-                    <label htmlFor="email">current location</label>
-                    <input type="text" placeholder="Enter your current location" id="email" />
-                </div>
+  };
 
-                <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="text" placeholder="Enter email address" id="email" />
-                </div>
+  return (
+    <div className="login_page">
+      <h2>Register</h2>
 
+      <form>
+        <div>
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            placeholder="Enter your fullname"
+            id="fullname"
+            name="fullname"
+            value={values.fullname}
+            onChange={handleChange}
+        
 
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter password" id="password"/>
-                </div>
-
-                
-                <div className="row">
-                    <button type="submit">Register</button>
-                </div>
-            </form>
-
-            <p>Already an account? <Link to="/login">Login</Link> </p>
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="name">username</label>
+          <input
+            type="text"
+            placeholder="Enter your username"
+            id="username"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">current location</label>
+          <input
+            type="text"
+            placeholder="Enter your current location"
+            id="location"
+            name="location"
+            value={values.location}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="text"
+            placeholder="Enter email address"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter password"
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="row">
+          <button type="submit">Register</button>
+        </div>
+      </form>
+
+      <p>
+        Already an account? <Link to="/login">Login</Link>{" "}
+      </p>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
